5c57c54a4332adf2718ed0f47766c9fd7e267d8c
Jimmy Kong <jimmyatwork368@gmail.com>
2024-09-20 22:56:22 -0700

    done: add documentation for data members in myStack.hpp

--- myStack.hpp
+++ myStack.hpp
@@ -15,17 +15,33 @@ private:
     int topIndex;
 
 public:
+    // Default constructor
+    // RETURN: a MyStack object initialized with topIndex = -1
     MyStack();
 
+    // Accept an int and adds it to the top of the stack, returning true if it was added, else false
+    // PRE: val is an integer
+    // POST: val is unchanged
+    // RETURN: a boolean value indicating whether val is added to the top of the stack
     bool push(int val);
 
+    // Decrement the instance variable which stores the index of the "top" element
+    // RETURN: does not return any value
     void pop();
 
+    // Return (without removing) the int on the "top" of the stack
+    // RETURN: top element of the stack as an int
     int top() const;
 
+    // Return true if the stack is empty, else false
+    // RETURN: a boolean value indicating whether stack is empty
     bool empty() const;
 
+    // Return true if the stack is full, else false
+    // RETURN: a boolean value indicating whether stack is full
     bool full() const;
 
+    // Print the contents of the stack (bottom of the stack to the top) to standard output and return the printed message
+    // RETURN: a string representation of the stack contents
     string print() const;
 };
